c3c5c601bd961528d96e57e8d959c8482787a3d6
Permas-Teo <permasteo@gmail.com>
2021-08-03 20:08:59 +0800

    add more sample products

--- src/reducers/product/productActions.js
+++ src/reducers/product/productActions.js
@@ -25,6 +25,7 @@ export const PRODUCT_FAILURE = "PRODUCT_FAILURE";
 const products = [
   {
     _id: 0,
+    filename: "GrabFood $20",
     description:
       "Your favourite food delivered hot and fresh! Satisfy your cravings by getting the food you love from your favourite restaurants. GrabFood is available islandwide. Enjoy this $20 QR Voucher.",
     origin: "Singapore",
@@ -35,6 +36,7 @@ const products = [
   },
   {
     _id: 1,
+    filename: "Courts $20",
     description:
       "Singapore’s Leading Consumer Electronics, IT & Furniture Store. Enjoy this $20 QR Voucher.",
     origin: "Singapore",
@@ -46,6 +48,7 @@ const products = [
   },
   {
     _id: 2,
+    filename: "Nike $20",
     description: "Just Do It! Enjoy this $20 QR Voucher.",
     origin: "Singapore",
     price: 10,
@@ -56,13 +59,106 @@ const products = [
   },
   {
     _id: 3,
+    filename: "Adidas $20",
     description:
       "Shop for adidas shoes, clothing and collections, adidas Originals, Running, Football, Training and more on the official adidas SG website. Enjoy this $20 QR Voucher.",
     origin: "Singapore",
-    price: 10000,
+    price: 10,
     standard: "New",
     thumb: "https://www.changicitypoint.com.sg/images/Adidas_logo.jpg",
     type: "bracelet",
+  },
+  {
+    _id: 4,
+    filename: "Redmart $15",
+    description:
+      "Buy Groceries At The Best Prices Online With Our Price Match Promise On RedMart. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb: "https://www.wogi.sg/uploads/brands/88/list_logo.jpg",
+    type: "stone",
+  },
+  {
+    _id: 5,
+    filename: "Deliveroo $15",
+    description:
+      "The best local restaurants and takeaways are here to deliver. Order on Deliveroo today! Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb:
+      "https://cdn2.downdetector.com/static/uploads/logo/deliveroo_2_SLJPKaS.png",
+    type: "stone",
+  },
+  {
+    _id: 6,
+    filename: "GrabFood $15",
+    description:
+      "Your favourite food delivered hot and fresh! Satisfy your cravings by getting the food you love from your favourite restaurants. GrabFood is available islandwide. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb: "https://assets.nst.com.my/images/articles/grabfooda_1601356379.jpg",
+    type: "stone",
+  },
+  {
+    _id: 7,
+    filename: "Courts $15",
+    description:
+      "Singapore’s Leading Consumer Electronics, IT & Furniture Store. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb:
+      "https://www.courts.com.sg/media/wysiwyg/courts-gift-card-620x420px.png",
+    type: "stone",
+  },
+  {
+    _id: 8,
+    filename: "Redmart $15",
+    description:
+      "Buy Groceries At The Best Prices Online With Our Price Match Promise On RedMart. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb: "https://www.wogi.sg/uploads/brands/88/list_logo.jpg",
+    type: "ring",
+  },
+  {
+    _id: 9,
+    filename: "Deliveroo $15",
+    description:
+      "The best local restaurants and takeaways are here to deliver. Order on Deliveroo today! Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb:
+      "https://cdn2.downdetector.com/static/uploads/logo/deliveroo_2_SLJPKaS.png",
+    type: "ring",
+  },
+  {
+    _id: 10,
+    filename: "Courts $15",
+    description:
+      "Singapore’s Leading Consumer Electronics, IT & Furniture Store. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb:
+      "https://www.courts.com.sg/media/wysiwyg/courts-gift-card-620x420px.png",
+    type: "ring",
+  },
+  {
+    _id: 11,
+    filename: "GrabFood $15",
+    description:
+      "Your favourite food delivered hot and fresh! Satisfy your cravings by getting the food you love from your favourite restaurants. GrabFood is available islandwide. Enjoy this $15 QR Voucher.",
+    origin: "Singapore",
+    price: 8,
+    standard: "New",
+    thumb: "https://assets.nst.com.my/images/articles/grabfooda_1601356379.jpg",
+    type: "ring",
   },
 ];
 export const fetchProducts = () => {
