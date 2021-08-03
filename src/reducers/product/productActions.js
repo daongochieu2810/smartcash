d35aa2047ffd4f505b4fb5ccff449ebc5557d18a
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2021-08-03 17:29:56 +0800

    Add sample data

--- src/reducers/product/productActions.js
+++ src/reducers/product/productActions.js
@@ -16,29 +16,46 @@ export const PRODUCT_FAILURE = "PRODUCT_FAILURE";
  * }
  *
  */
+// 3 types:
+/**
+ * bracelet -> 1st section
+ * stones -> 2nd sectiom
+ * rings -> 3rd
+ */
+const products = [
+  {
+    _id: 0,
+    description: "Some meaningful things",
+    origin: "Singapore",
+    price: 7000,
+    standard: "New",
+    thumb: "https://imagesample.com",
+    type: "bracelet",
+  },
+];
 export const fetchProducts = () => {
   return async (dispatch) => {
     dispatch({
       type: PRODUCT_LOADING,
     });
     try {
-      const response = await timeoutPromise(
-        fetch(`${API_URL}/product`, {
-          method: "GET",
-        })
-      );
+      // const response = await timeoutPromise(
+      //   fetch(`${API_URL}/product`, {
+      //     method: "GET",
+      //   })
+      // );
 
-      if (!response.ok) {
-        dispatch({
-          type: PRODUCT_FAILURE,
-        });
-        throw new Error("Something went wrong!, can't get the products");
-      }
-      const resData = await response.json();
-      console.log(resData);
+      // if (!response.ok) {
+      //   dispatch({
+      //     type: PRODUCT_FAILURE,
+      //   });
+      //   throw new Error("Something went wrong!, can't get the products");
+      // }
+      // const resData = await response.json();
+      // console.log(resData);
       dispatch({
         type: FETCH_PRODUCTS,
-        products: resData.content,
+        products: products,
       });
     } catch (err) {
       throw err;
